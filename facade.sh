33a728c3fa15a8840b5c76c3d0b0e8e6e33da970
Roman Saveljev <roman.saveljev@haltian.com>
2015-10-09 00:06:41 +0300

    More functions to enter junest

--- facade.sh
+++ facade.sh
@@ -8,23 +8,43 @@ __de_sanitize() {
     PATH=${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/3rdparty/junest/bin:$PATH ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/sanitizer/sanitize
 }
 
-__de_privileged_junest() {
-    JUNEST_HOME=${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/layers/basement ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/3rdparty/junest/bin/junest -f -- $@
+__de_unprivileged_junest() {
+    local mountpoint=$1
+    shift
+    JUNEST_HOME=$mountpoint ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/3rdparty/junest/bin/junest -- "$@"
 }
 
-__de_unprivileged_junest() {
-    JUNEST_HOME=${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/layers/basement ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/3rdparty/junest/bin/junest -- $@
+__de_privileged_junest() {
+    local mountpoint=$1
+    shift
+    JUNEST_HOME=$mountpoint ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/3rdparty/junest/bin/junest -f -- "$@"
+}
+
+__de_privileged_basement_junest() {
+    __de_privileged_junest ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/layers/basement "$@"
+}
+
+__de_unprivileged_basement_junest() {
+    __de_unprivileged_junest ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/layers/basement "$@"
 }
 
 __de_install_basement() {
-    __de_privileged_junest yaourt -S $@
+    __de_privileged_basement_junest yaourt -S "$@"
 }
 
 __de_upgrade_basement() {
     # Hangs due to https://github.com/fsquillace/junest/issues/116
-    #__de_privileged_junest pacman-key --populate
-    #__de_privileged_junest pacman-key --refresh-keys
-    __de_privileged_junest pacman -Syu --noconfirm
+    #__de_privileged_basement_junest pacman-key --populate
+    #__de_privileged_basement_junest pacman-key --refresh-keys
+    __de_privileged_basement_junest pacman -Syu --noconfirm
+}
+
+__de_privileged_all_junest() {
+    __de_privileged_junest ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/mnt/all "$@"
+}
+
+__de_unprivileged_all_junest() {
+    __de_unprivileged_junest ${BASE_DE_WORKSPACE_FOLDER}/.cross-dev/mnt/all "$@"
 }
 
 __de_environment_config() {

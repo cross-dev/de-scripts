f0db17f19a4da6d7f0a902d2928609c1b96024d9
Roman Saveljev <roman.saveljev@haltian.com>
2015-10-19 00:11:32 +0300

    Implement buildpkg for the facade

--- facade.py
+++ facade.py
@@ -11,56 +11,96 @@ import sys
 root = deroot.check_and_die()
 base = os.path.dirname(os.path.realpath(__file__))
 
-def extend_env():
-    new_path = "{0}:{1}".format(os.path.join(base, "..", "3rdparty", "junest", "bin"), os.environ["PATH"])
-    has_junest_env = dict(os.environ, PATH=new_path)
-    return has_junest_env
+def junest_to_path(env):
+    new_path = "{0}:{1}".format(os.path.join(base, "..", "3rdparty", "junest", "bin"), env["PATH"])
+    return dict(env, PATH=new_path)
+
+class JuNest:
+    def __init__(self, mountpoint=os.path.join(base, "..", "mnt", "all"), env=os.environ):
+        self.env = junest_to_path(env)
+        self.env = dict(self.env, JUNEST_HOME=mountpoint)
+
+    def _run(self, junest_args, shell_command, cwd):
+        runner = os.path.join(base, "junest-run")
+        p = subprocess.Popen([runner] + junest_args + ['--'] + shell_command, cwd=cwd, env=self.env)
+        return p.wait()
+
+    def run_privileged(self, shell_command, cwd="."):
+        return self._run(junest_args=["-f"], shell_command=shell_command, cwd=cwd)
+
+    def run_unprivileged(self, shell_command, cwd="."):
+        return self._run(junest_args=[], shell_command=shell_command, cwd=cwd)
 
 def sanitize(args):
-    has_junest_env = extend_env()
     sanitize = os.path.join(base, "sanitize")
-    p = subprocess.Popen(sanitize, cwd=root, env=has_junest_env)
+    p = subprocess.Popen(sanitize, cwd=root, env=junest_to_path(os.environ))
     return p.wait()
-
-def junest_run(mountpoint, junest_args, shell_command, cwd):
-    env = dict(extend_env(), JUNEST_HOME=mountpoint)
-    runner = os.path.join(base, "junest-run")
-    p = subprocess.Popen([runner] + junest_args + ['--'] + shell_command, cwd=cwd, env=env)
-    #p = subprocess.Popen(["/bin/bash"])
-    return p.wait()
-
-def junest_privileged_run(mountpoint, shell_command, cwd):
-    return junest_run(mountpoint=mountpoint, junest_args=["-f"], shell_command=shell_command, cwd=cwd)
-
-def junest_unprivileged_run(mountpoint, shell_command, cwd):
-    return junest_run(mountpoint=mountpoint, junest_args=[], shell_command=shell_command, cwd=cwd)
-
-def junest_privileged_base_run(shell_command, cwd="."):
-    mountpoint = os.path.join(base, "..", "layers", "base")
-    return junest_privileged_run(mountpoint=mountpoint, shell_command=shell_command, cwd=cwd)
-
-def junest_privileged_all_run(shell_command, cwd="."):
-    mountpoint = os.path.join(base, "..", "mnt", "all")
-    return junest_privileged_run(mountpoint=mountpoint, shell_command=shell_command, cwd=cwd)
-
-def junest_unprivileged_all_run(shell_command, cwd="."):
-    mountpoint = os.path.join(base, "..", "mnt", "all")
-    return junest_unprivileged_run(mountpoint=mountpoint, shell_command=shell_command, cwd=cwd)
 
 def shell(args):
-    return junest_unprivileged_all_run(args.cmd)
+    junest = JuNest()
+    return junest.run_unprivileged(args.cmd)
 
 def sudo(args):
-    return junest_privileged_all_run(args.cmd)
+    junest = JuNest()
+    return junest.run_privileged(args.cmd)
 
 def tools(args):
     input = open(os.path.join(base, "tools.sh.in"), "r")
     print(input.read().replace("%FACADE_PATH%", os.path.realpath(__file__)).replace("%DE_ROOT%", root))
     input.close()
 
-
 def menuconfig(args):
-    return junest_unprivileged_all_run(["kconfig-nconf", "Kconfig"], cwd=root)
+    junest = JuNest(cwd=root)
+    return junest.run_unprivileged(["kconfig-nconf", "Kconfig"])
+
+def buildpkg(args):
+    _project = args.project
+    _version = subprocess.check_output([os.path.join(base, "get-project-version"), root, _project]).rstrip()
+    _base = subprocess.check_output([os.path.join(base, "get-project-base"), root, _project]).rstrip()
+    env = dict(os.environ, _version=_version, _project=_project, _base=_base)
+    junest = JuNest(env=env)
+    _args = [os.path.join(base, "buildpkg")]
+    if args.ignorearch:
+        _args += ["--ignorearch"]
+    if args.nodeps:
+        _args += ["--nodeps"]
+    if args.force:
+        _args += ["--force"]
+    if args.geninteg:
+        _args += ["--geninteg"]
+    if args.install:
+        _args += ["--install"]
+    if args.log:
+        _args += ["--log"]
+    if args.nocolor:
+        _args += ["--nocolor"]
+    if args.rmdeps:
+        _args += ["--rmdeps"]
+    if args.repackage:
+        _args += ["--repackage"]
+    if args.syncdeps:
+        _args += ["--syncdeps"]
+    if args.check:
+        _args += ["--check"]
+    if args.config is not None:
+        _args += ["--config", args.config]
+    if args.noarchive:
+        _args += ["--noarchive"]
+    if args.nocheck:
+        _args += ["--nocheck"]
+    if args.noprepare:
+        _args += ["--noprepare"]
+    if args.asdeps:
+        _args += ["--asdeps"]
+    if args.needed:
+        _args += ["--needed"]
+    if args.noconfirm:
+        _args += ["--noconfirm"]
+    if args.noprogressbar:
+        _args += ["--noprogressbar"]
+
+    return junest.run_privileged(_args, cwd=root)
+
 
 ##### PROGRAM MAIN FLOW
 
@@ -84,6 +124,46 @@ parser_tools.set_defaults(func=tools)
 
 parser_config = subparsers.add_parser("menuconfig", help="Run graphical configuration tool")
 parser_config.set_defaults(func=menuconfig)
+
+parser_buildpkg = subparsers.add_parser("buildpkg", help="Build project package from the universal PKGBUILD")
+parser_buildpkg.set_defaults(func=buildpkg)
+parser_buildpkg.add_argument("--ignorearch", "-A", help="Ignore incomplete arch field in PKGBUILD", action="store_true")
+#parser_buildpkg.add_argument("--clean", "-c", help="Clean up work files after build", action="store_true")
+#parser_buildpkg.add_argument("--cleanbuild", "-C", help="Remove $srcdir/ dir before building the package", action="store_true")
+parser_buildpkg.add_argument("--nodeps", "-d", help="Skip all dependency checks", action="store_true")
+#parser_buildpkg.add_argument("--noextract", "-e", help="Do not extract source files (use existing $srcdir/ dir)", action="store_true")
+parser_buildpkg.add_argument("--force", "-f", help="Overwrite existing package", action="store_true")
+parser_buildpkg.add_argument("--geninteg", "-g", help="Generate integrity checks for source files", action="store_true")
+parser_buildpkg.add_argument("--install", "-i", help="Install package after successful build", action="store_true")
+parser_buildpkg.add_argument("--log", "-L", help="Log package build process", action="store_true")
+parser_buildpkg.add_argument("--nocolor", "-m", help="Disable colorized output messages", action="store_true")
+#parser_buildpkg.add_argument("--nobuild", "-o", help="Download and extract files only", action="store_true")
+#parser_buildpkg.add_argument("-p", dest="file", help="Use an alternate build script (instead of 'PKGBUILD')")
+parser_buildpkg.add_argument("--rmdeps", "-r", help="Remove installed dependencies after a successful build", action="store_true")
+parser_buildpkg.add_argument("--repackage", "-R", help="Repackage contents of the package without rebuilding", action="store_true")
+parser_buildpkg.add_argument("--syncdeps", "-s", help="Install missing dependencies with pacman", action="store_true")
+#parser_buildpkg.add_argument("--source", "-S", help="Generate a source-only tarball without downloaded sources", action="store_true")
+#parser_buildpkg.add_argument("--version", "-V", help="Show version information and exit", action="store_true")
+#parser_buildpkg.add_argument("--allsource", help="Generate a source-only tarball including downloaded sources", action="store_true")
+parser_buildpkg.add_argument("--check", help="Run the check() function in the PKGBUILD", action="store_true")
+parser_buildpkg.add_argument("--config", help="Use an alternate config file (instead of '/etc/makepkg.conf')")
+#parser_buildpkg.add_argument("--holdver", help="Do not update VCS sources", action="store_true")
+#--key
+parser_buildpkg.add_argument("--noarchive", help="Do not create package archive", action="store_true")
+parser_buildpkg.add_argument("--nocheck", help="Do not run the check() function in the PKGBUILD", action="store_true")
+parser_buildpkg.add_argument("--noprepare", help="Do not run the prepare() function in the PKGBUILD", action="store_true")
+#--nosign
+#--pkg
+#--sign
+#--skipchecksums
+#--skipinteg
+#--skippgpcheck
+#--verifysource
+parser_buildpkg.add_argument("--asdeps", help="Install packages as non-explicitly installed", action="store_true")
+parser_buildpkg.add_argument("--needed", help="Do not reinstall the targets that are already up to date", action="store_true")
+parser_buildpkg.add_argument("--noconfirm", help="Do not ask for confirmation when resolving dependencies", action="store_true")
+parser_buildpkg.add_argument("--noprogressbar", help="Do not show a progress bar when downloading files", action="store_true")
+parser_buildpkg.add_argument("project", help="Project name as defined by manifest")
 
 args = parser.parse_args()
 sys.exit(args.func(args))
